284e15937cccccd1c722802f0bdd51f51e06ac59
Sol Boucher <slb1566@rit.edu>
2013-11-09 08:18:44 -0500

    Lane actually did this! Also, we moved SensorConstants into api

---
+++ desktop/src/api/SensorConstants.java
@@ -0,0 +1,22 @@
+package api;
+
+class SensorConstants{
+	public static final int TYPE_ACCELEROMETER = 1;
+	public static final int TYPE_AMBIENT_TEMPERATURE = 13;
+	public static final int TYPE_GAME_ROTATION_VECTOR = 15;
+	public static final int TYPE_GEOMAGNETIC_ROTATION_VECTOR = 20;
+	public static final int TYPE_GRAVITY = 9;
+	public static final int TYPE_GYROSCOPE = 4;
+	public static final int TYPE_GYROSCOPE_UNCALIBRATED = 16;
+	public static final int TYPE_LIGHT = 5;
+	public static final int TYPE_LINEAR_ACCELERATION = 10;
+	public static final int TYPE_MAGNETIC_FIELD = 2;
+	public static final int TYPE_MAGNETIC_FIELD_UNCALIBRATED = 14;
+	public static final int TYPE_PRESSURE = 6;
+	public static final int TYPE_PROXIMITY = 8;
+	public static final int TYPE_RELATIVE_HUMIDITY = 12;
+	public static final int TYPE_ROTATION_VECTOR = 11;
+	public static final int TYPE_SIGNIFICANT_MOTION = 17;
+	public static final int TYPE_STEP_COUNTER = 19;
+	public static final int TYPE_STEP_DETECTOR = 18;
+}
