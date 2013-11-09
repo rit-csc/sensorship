f43bbfa6d98661f779a3b62cf0e2d586193c5a2c
bitbanger <lxl5734@rit.edu>
2013-11-09 15:11:38 -0500

    YAYYYYY PONG

--- desktop/src/game/GViewControl.java
+++ desktop/src/game/GViewControl.java
@@ -8,23 +8,45 @@ import java.util.*; //Observable, Observer
 
 import api.Registrar;
 
+class Ball {
+	public int x;
+	public int y;
+	public double xVel;
+	public double yVel;
+	
+	public Ball(int x, int y, double xVel, double yVel) {
+		this.x = x;
+		this.y = y;
+		this.xVel = xVel;
+		this.yVel = yVel;
+	}
+}
+
 public class GViewControl {
 	private static final long SLEEP_TIME_MS = 100;
 	public static final int WINDOW_WIDTH = 500;
 	public static final int WINDOW_HEIGHT = 500;
-	private static final int SHIP_RAD = 30;
+	private static final int BALL_RAD = 30;
+	
+	public static final double BALL_SPEED = 8.0;
+	
+	// 75 degrees
+	public static final double MAX_BOUNCE_ANGLE = 5 * Math.PI/12;
 
 	private static ArrayList<Player> ships = new ArrayList<Player>();
 	private static Graphics drawable = null;
 
     public static void main(String[] args) {
-    	//System.setProperty("java.util.Arrays.useLegacyMergeSort", "true");
-
+   		
     	//create outer frame for game display
 		JFrame frame = new JFrame("SensorSHIP");
 		frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		frame.setLocation(100, 100);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		final Ball ball = new Ball(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, (float)(BALL_SPEED), 0.0f);
+		
 		
 		//get content pane of the frame
 		frame.setContentPane(new JPanel() {
@@ -39,7 +61,55 @@ public class GViewControl {
 					// drawable.fillOval(coords[0], coords[1], SHIP_RAD, SHIP_RAD);
 					float[] xAndY = each.xAndY();
 					drawable.drawRect((int)xAndY[0], (int)xAndY[1], Player.PADDLE_WIDTH, Player.PADDLE_HEIGHT);
+					
 				}
+				
+				ball.x = (int)(float)(ball.x + ball.xVel);
+				ball.y = (int)(float)(ball.y + ball.yVel);
+				
+				if(ball.y < 0 + BALL_RAD || ball.y > WINDOW_HEIGHT - BALL_RAD) {
+					ball.yVel = -ball.yVel;
+				}
+				
+				if((ball.x - BALL_RAD/2) < Player.PADDLE_WIDTH) {
+					float[] p0coords = ships.get(0).xAndY();
+					double p0middle = (p0coords[1] + Player.PADDLE_HEIGHT/2);
+					
+					double distFromP0Middle = (double)ball.y - p0middle;
+					
+					if(Math.abs(distFromP0Middle) < Player.PADDLE_HEIGHT/2) {
+						System.out.println("WE HIT THE PADDLE (p0)!");
+						// We hit the paddle
+						double normalizedRelativeIntersectionY = (distFromP0Middle/(Player.PADDLE_HEIGHT/2));
+						double bounceAngle = normalizedRelativeIntersectionY * MAX_BOUNCE_ANGLE;
+						
+						ball.xVel = BALL_SPEED * Math.cos(bounceAngle);
+						ball.yVel = BALL_SPEED * -Math.sin(bounceAngle);
+						
+						System.out.printf("Set new xvel, yvel to %f, %f\n", ball.xVel, ball.yVel);
+					}
+				}
+				
+				if((ball.x + BALL_RAD/2) > (WINDOW_WIDTH - Player.PADDLE_WIDTH)) {
+					float[] p1coords = ships.get(1).xAndY();
+					double p1middle = (p1coords[1] + Player.PADDLE_HEIGHT/2);
+					
+					double distFromP1Middle = (double)ball.y - p1middle;
+					
+					if(Math.abs(distFromP1Middle) < Player.PADDLE_HEIGHT/2) {
+						System.out.println("WE HIT THE PADDLE (p1)!");
+						// We hit the paddle
+						double normalizedRelativeIntersectionY = (distFromP1Middle/(Player.PADDLE_HEIGHT/2));
+						double bounceAngle = normalizedRelativeIntersectionY * MAX_BOUNCE_ANGLE;
+						
+						ball.xVel = BALL_SPEED * -Math.cos(bounceAngle);
+						ball.yVel = BALL_SPEED * -Math.sin(bounceAngle);
+						
+						System.out.printf("Set new xvel, yvel to %f, %f\n", ball.xVel, ball.yVel);
+					}
+				}
+				
+				drawable.fillOval(ball.x, ball.y, BALL_RAD, BALL_RAD);
 			}
 		});
 		Container container = frame.getContentPane();
