7eb66f4f4f1f5c1ad60b90f77bb0016e0d8be9c8
Sol Boucher <slb1566@rit.edu>
2013-11-08 21:15:33 -0500

    Start Android project

---
+++ src/edu/rit/cs/csc/sensorship/Sensorship.java
@@ -0,0 +1,15 @@
+package edu.rit.cs.csc.sensorship;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Sensorship extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
