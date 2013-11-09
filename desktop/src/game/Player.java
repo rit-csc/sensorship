efe013fb6acecc0fa4dab9587f704bcf78f7a323
khipkin <kah5368@rit.edu>
2013-11-09 07:26:13 -0500

    FUCK

--- desktop/src/game/Player.java
+++ desktop/src/game/Player.java
@@ -1,44 +1,13 @@
 package game;
-import java.lang.String;
 
 public class Player {
-    private int id;
 	private int curr_x;
 	private int curr_y;
 	private String player_name;
-	private int velocity;
-	private int heading;
 
     public Player(String player_name, int init_x, int init_y) {
     	this.player_name = player_name;
     	this.curr_x = init_x;
     	this.curr_y = init_y;
-    	this.velocity = 0;
-    	this.heading = 0;
-    }
-
-    public void setHeading(int heading) {
-    	this.heading = heading;
-    }
-
-    public int getHeading() {
-        return this.heading;
-    }
-
-    public void setVelocity(int velocity) {
-        this.velocity = velocity;
-    }
-
-    public int getVelocity() {
-        return this.velocity;
-    }
-
-    public void setPos(int x, int y) {
-        this.curr_x = x;
-        this.curr_y = y;
-    }
-
-    public String toString() {
-    	return "{" + player_name + ":" + curr_x + "," + curr_y + "," + heading + "}";
     }
 }
--- desktop/src/game/ShipData.java
+++ desktop/src/game/ShipData.java
@@ -1,6 +1,5 @@
 package game;
 import java.util.*;
-import java.io.*;
 
 public class ShipData {
 	private final int MIN_X_COOR = 0;
@@ -10,59 +9,18 @@ public class ShipData {
 	private ArrayList<Player> players;
 
     public ShipData(ArrayList<String> players) {
-    	this.players = new ArrayList<Player>();
-        int j = 0;
+    	int j = 0;
     	for (int i = 0; i < players.size(); i++) {
     		this.players.add(new Player(players.get(i),i*10,j*5));
     		j+=1;
     	}
+    	System.out.println(""+this.players);
     }
 
-    public ArrayList<Player> getPlayers() {
-    	return this.players;
-    }
-
-    public void updateHeading(Player player, int heading) {
-    	player.setHeading(heading);
-    }
-
-    public void updateVelocity(Player player, int velocity) {
-        player.setVelocity(velocity);
-    }
-
-    public void updateAllPos() {
-        for (Player p : this.players) {
-            int x;
-            int y;
-            x = p.getVelocity()*(int)Math.floor(Math.cos(p.getHeading()));
-            if (x < MIN_X_COOR) {
-                x = MAX_X_COOR - x;
-            }
-            else if (x > MAX_X_COOR) {
-                x = x % MAX_X_COOR;
-            }
-            y = p.getVelocity()*(int)Math.floor(Math.sin(p.getHeading()));
-            if (y < MIN_Y_COOR) {
-                y = MAX_Y_COOR - y;
-            }
-            else if (y > MAX_Y_COOR) {
-                y = y % MAX_Y_COOR;
-            }
-            System.out.println(""+x+","+y);
-            p.setPos(x,y);
-        }
-    }
-
-    public static void main(String[] args) {
+    public static void main(String args[]) {
     	ArrayList<String> players = new ArrayList<String>();
     	players.add("Eric");
     	players.add("Katie");
-    	ShipData game = new ShipData(players);
-        game.updateAllPos();
-    	System.out.println(""+game.getPlayers());
-    	game.updateHeading(game.getPlayers().get(0),90);
-        game.updateVelocity(game.getPlayers().get(0),10);
-        game.updateAllPos();
-    	System.out.println(""+game.getPlayers());
+    	ShipData ship = new ShipData(players);
     }
 }
