0eeef8ff971bda330220a4bae5ca59eb8970dea2
EricFalkenberg <exf4789@rit.edu>
2013-11-09 06:25:53 -0500

    moving things

--- desktop/src/game/Player.java
+++ desktop/src/game/Player.java
@@ -1,13 +1,26 @@
 package game;
+import java.lang.String;
 
 public class Player {
 	private int curr_x;
 	private int curr_y;
 	private String player_name;
+	private int velocity;
+	private int heading;
 
     public Player(String player_name, int init_x, int init_y) {
     	this.player_name = player_name;
     	this.curr_x = init_x;
     	this.curr_y = init_y;
+    	this.velocity = 0;
+    	this.heading = 0;
+    }
+
+    public void setHeading(int heading) {
+    	this.heading = heading;
+    }
+
+    public String toString() {
+    	return "{" + player_name + ":" + curr_x + "," + curr_y + "}";
     }
 }
--- desktop/src/game/ShipData.java
+++ desktop/src/game/ShipData.java
@@ -1,5 +1,6 @@
 package game;
 import java.util.*;
+import java.io.*;
 
 public class ShipData {
 	private final int MIN_X_COOR = 0;
@@ -14,13 +15,23 @@ public class ShipData {
     		this.players.add(new Player(players.get(i),i*10,j*5));
     		j+=1;
     	}
-    	System.out.println(""+this.players);
     }
 
-    public static void main(String args[]) {
+    public ArrayList<Player> getPlayers() {
+    	return this.players;
+    }
+
+    public void updateHeading(Player player, int heading) {
+    	player.setHeading(heading);
+    }
+
+    public static void main(String[] args) {
     	ArrayList<String> players = new ArrayList<String>();
     	players.add("Eric");
     	players.add("Katie");
-    	ShipData ship = new ShipData(players);
+    	ShipData game = new ShipData(players);
+    	System.out.println(""+game.getPlayers());
+    	game.updateHeading(game.getPlayers().get(0),10);
+    	System.out.println(""+game.getPlayers());
     }
 }
