b3c2e8240c8996e7ab100c5ac309860b074cec29
EricFalkenberg <exf4789@rit.edu>
2013-11-09 07:35:49 -0500

    finished up shipdata model for ship game

--- desktop/src/game/Player.java
+++ desktop/src/game/Player.java
@@ -1,13 +1,62 @@
 package game;
 
+/**
+ *   Player class for representing a player in-game
+ */
 public class Player {
 	private int curr_x;
 	private int curr_y;
 	private String player_name;
+	private int x_velocity;
+        private int y_velocity;
 
+    /**
+     *   Constructor
+     */
     public Player(String player_name, int init_x, int init_y) {
     	this.player_name = player_name;
     	this.curr_x = init_x;
     	this.curr_y = init_y;
+    	this.x_velocity = 0;
+        this.y_velocity = 0;
+    }
+
+    /**
+     *   sets the velocities of a player
+     */
+    public void setVelocity(int x, int y) {
+        this.x_velocity = x;
+        this.y_velocity = y;
+    }
+
+    /**
+     *   retrieves a velocity for a player
+     */
+    public int[] getVelocity() {
+        int velocity[] = {this.x_velocity,this.y_velocity};
+        return velocity;
+    }
+
+    /**
+     *   sets a new position for a player
+     */
+    public void setPos(int x, int y) {
+        this.curr_x = x;
+        this.curr_y = y;
+    }
+
+    /**
+     *    gets the position of a player
+     */
+    public int[] getPos() {
+        int pos[] = {this.curr_x,this.curr_y};
+        return pos;
+    }
+
+    /**
+     *     used for printing a character object to the screen.
+     */
+    public String toString() {
+    	return "{" + player_name + ":" + curr_x + "," + curr_y + "}";
     }
 }
--- desktop/src/game/ShipData.java
+++ desktop/src/game/ShipData.java
@@ -1,6 +1,9 @@
 package game;
 import java.util.*;
 
+/**
+ *  Data model for our ship game.
+ */
 public class ShipData {
 	private final int MIN_X_COOR = 0;
 	private final int MIN_Y_COOR = 0;
@@ -8,6 +11,9 @@ public class ShipData {
 	private final int MAX_Y_COOR = 100;
 	private ArrayList<Player> players;
 
+    /**
+     *  Constructor
+     */
     public ShipData(ArrayList<String> players) {
     	int j = 0;
     	for (int i = 0; i < players.size(); i++) {
@@ -17,10 +23,43 @@ public class ShipData {
     	System.out.println(""+this.players);
     }
 
-    public static void main(String args[]) {
-    	ArrayList<String> players = new ArrayList<String>();
-    	players.add("Eric");
-    	players.add("Katie");
-    	ShipData ship = new ShipData(players);
+    /**
+     *  Returns the list of players currently playing.
+     */
+    public ArrayList<Player> getPlayers() {
+    	return this.players;
+    }
+
+    /**
+     *   Updates the velocity of a specified player
+     */
+    public void updateVelocity(Player player, int x, int y) {
+        player.setVelocity(x,y);
+    }
+
+    /**
+     *   Takes all player and updates their positions
+     *   based on x and y velocities.
+     */
+    public void updateAllPos() {
+        for (Player p : this.players) {
+            int x;
+            int y;
+            x = p.getPos()[0] + p.getVelocity()[0];
+            if (x < MIN_X_COOR) {
+                x = MAX_X_COOR + x;
+            }
+            else if (x > MAX_X_COOR) {
+                x = x % MAX_X_COOR;
+            }
+            y = p.getPos()[1] + p.getVelocity()[1];
+            if (y < MIN_Y_COOR) {
+                y = MAX_Y_COOR + y;
+            }
+            else if (y > MAX_Y_COOR) {
+                y = y % MAX_Y_COOR;
+            }
+            p.setPos(x,y);
+        }
     }
 }
