67c45cb2a3a660ef184b617ff374ed2e1b5c9546
khipkin <kah5368@rit.edu>
2013-11-09 04:55:22 -0500

    Constants copied from Sensor class

---
+++ desktop/src/edu/rit/cs/csc/sensorship/deltaforce/SensorConstants.java
@@ -0,0 +1,22 @@
+class SensorConstants{
+
+	public final int TYPE_ACCELEROMETER = 1;
+	public final int TYPE_ALL = -1;
+	public final int TYPE_AMBIENT_TEMPERATURE = 13;
+	public final int TYPE_GAME_ROTATION_VECTOR = 15;
+	public final int TYPE_GEOMAGNETIC_ROTATION_VECTOR = 20;
+	public final int TYPE_GRAVITY = 9;
+	public final int TYPE_GYROSCOPE = 4;
+	public final int TYPE_GYROSCOPE_UNCALIBRATED = 16;
+	public final int TYPE_LIGHT = 5;
+	public final int TYPE_LINEAR_ACCELERATION = 10;
+	public final int TYPE_MAGNETIC_FIELD = 24;
+	public final int TYPE_MAGNETIC_FIELD_UNCALIBRATED = 14;
+	public final int TYPE_PRESSURE = 6;
+	public final int TYPE_PROXIMITY = 8;
+	public final int TYPE_RELATIVE_HUMIDITY = 12;
+	public final int TYPE_ROTATION_VECTOR = 11;
+	public final int TYPE_SIGNIFICANT_MOTION = 17;
+	public final int TYPE_STEP_COUNTER = 19;
+	public final int TYPE_STEP_DETECTOR = 18;
+}
